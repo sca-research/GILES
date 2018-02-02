421b1783c1dcaf9b5f2d60e8489e8d6ef7cfbcad
Scott Egerton <scott.egerton@bristol.ac.uk>
2018-02-02 12:55:36 +0000

    Updated license

--- src/Emulator_Interface.hpp
+++ src/Emulator_Interface.hpp
@@ -2,16 +2,16 @@
     This file is part of ELMO-2.
 
     ELMO-2 is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     ELMO-2 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Affero General Public License
     along with ELMO-2.  If not, see <http://www.gnu.org/licenses/>.
 */
 
@@ -21,7 +21,7 @@
     the interface to a specific emulator.
     @author Scott Egerton
     @date 2017
-    @copyright GNU General Public License Version 2+
+    @copyright GNU Affero General Public License Version 3+
 */
 
 #ifndef EMULATOR_INTERFACE_HPP
