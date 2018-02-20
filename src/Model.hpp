50f9e33c465b1ff8f5934e5ae3069b62ae28ba0e
Scott Egerton <scott.egerton@bristol.ac.uk>
2018-02-20 15:07:02 +0000

    Added a CMake generated Model factory class as well as a template model.
    The model factory was added to assist with the initialisation of Models
    so that the main logic does not need to be aware of which model(s) are
    in use.
    
    The CMake module added generates the Model_Factory class based on which
    models are available and specified in the CMakeLists.txt. This allows
    for selective compilation of models as well as making the process of
    adding new models easier.
    
    A template for what a model should look like has been added. This can be
    used to assist in adding new models.

--- src/Model.hpp
+++ src/Model.hpp
@@ -17,7 +17,7 @@
 
 /*!
     @file Model.hpp
-    @brief Contains the Model class which serves as a base case for mathmatical
+    @brief Contains the Model class which serves as a base case for mathematical
     models to be implemented.
     @author Scott Egerton
     @date 2017-2018
@@ -36,34 +36,35 @@ namespace ELMO2
 namespace Internal
 {
 //! @class Model
-//! @brief An abstract class that serves as a base class for mathmatical models
+//! @brief An abstract class that serves as a base class for mathematical models
 //! to be implemented, These models will generate the traces for the given
 //! program by making use of the Coefficients and the recorded Execution of the
 //! program.
 class Model
 {
 private:
-    //! The execution of the traget program as recorded by the Emulator.
+    //! The execution of the target program as recorded by the Emulator.
     const ELMO2::Internal::Execution m_execution;
 
     //! The Coefficients created by measuring real hardware traces.
-    const ELMO2::Internal::Coefficients m_coefficients;
+    const ELMO2::Internal::Coefficients& m_coefficients;
 
     //! @brief This function will add noise to the traces to simulate the
     //! background noise that would be picked up in real traces.
-    //! @ param The Traces to have the noise added to.
+    //! @param p_traces The Traces to have the noise added to.
     //! @returns The Traces with the added noise.
     const ELMO2::Internal::Traces& addNoise(ELMO2::Internal::Traces& p_traces);
 
 protected:
-    //! @brief The constructor needs to be provided with the recorded Execution
-    //! of the target program and the details from real world traces, the
-    //! Coefficients to be able to calculate the Traces for the target program.
-    //! This contructor is marked as protected as it should only be
-    //! called by derived classes to assist with initilisation.
+    //! @brief The constructor needs to be provided with the recorded
+    //! Execution of the target program and the details from real world
+    //! traces, the Coefficients, to be able to calculate the Traces for the
+    //! target program. This constructor is marked as protected as it should
+    //! only be called by derived classes to assist with initialisation.
     //! @param p_execution The recorded Execution of the target program,
     //! provided by the Emulator.
-    //! @param p_coefficients The loaded Coefficients from real hardware traces.
+    //! @param p_coefficients The loaded Coefficients from real hardware
+    //! traces.
     Model(const ELMO2::Internal::Execution& p_execution,
           const ELMO2::Internal::Coefficients& p_coefficients)
         : m_execution(p_execution), m_coefficients(p_coefficients)
@@ -72,16 +73,38 @@ protected:
     // 0??? And have a generate_traces()
     // method to do this instead?
 
+    //! @brief Retrieves the interaction terms used within the model. This is
+    //! required to ensure the Coefficients file provided all of the required
+    //! interaction terms.
+    //! @returns A list of required interaction terms as defined in the
+    //! derived class.
+    virtual const std::unordered_set<std::string>&
+    Get_Interaction_Terms() const = 0;
+
 public:
-    //! @brief Virtual deconstructor to ensure proper memory cleanup.
+    //! @brief Virtual destructor to ensure proper memory cleanup.
     //! @see https://stackoverflow.com/a/461224
-    virtual ~Model(); // TODO: Should this = default? Rule of ZERO?
-                      // http://en.cppreference.com/w/cpp/language/rule_of_three
+    virtual ~Model() = default; // TODO: Should this = default? Rule of
+    // ZERO? http://en.cppreference.com/w/cpp/language/rule_of_three
 
     //! @brief In derived classes, this function should contain the mathematical
     //! calculations that generate the Traces.
-    //! @returns The generted Traces for the target program.
-    virtual const ELMO2::Internal::Traces& GenerateTraces() = 0;
+    //! @returns The generated Traces for the target program.
+    virtual const ELMO2::Internal::Traces& Generate_Traces() const = 0;
+
+    //! @brief Ensures that all the interaction terms used within the model
+    //! are provided by the Coefficients.
+    //! @returns True if the all the interaction terms required by the model
+    //! are contained within the Coefficients.
+    bool Find_Interaction_Terms() const
+    {
+        const auto& models_terms = Get_Interaction_Terms();
+        const auto& coefficients_terms = m_coefficients.Get_Interaction_Terms();
+
+        return std::includes(coefficients_terms.begin(),
+                             coefficients_terms.end(), models_terms.begin(),
+                             models_terms.end());
+    }
 };
 } // namespace Internal
 } // namespace ELMO2
---
+++ src/Model_TEMPLATE.cpp
@@ -0,0 +1,61 @@
+/*
+    This file is part of ELMO-2.
+
+    ELMO-2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ELMO-2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with ELMO-2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*!
+    @file Model_TEMPLATE.hpp
+    @brief This file is a template. This can be used as a base for implementing
+    a mathematical model for generating traces of a given target program.
+    @author Scott Egerton
+    @date 2017-2018
+    @copyright GNU Affero General Public License Version 3+
+*/
+
+#include "Model_TEMPLATE.hpp"
+
+//! The list of interaction terms used by this model in order to generate
+//! traces.
+const std::unordered_set<std::string>
+    ELMO2::Internal::Model_TEMPLATE::m_required_interaction_terms = {
+        "Bit_Flip1",
+        "Bit_Flip1_Bit_Interactions",
+        "Bit_Flip2",
+        "Bit_Flip2_Bit_Interactions",
+        "Hamming_Distance_Operand1_Previous_Instruction",
+        "Hamming_Distance_Operand1_Subsequent_Instruction",
+        "Hamming_Distance_Operand2_Previous_Instruction",
+        "Hamming_Distance_Operand2_Subsequent_Instruction",
+        "Hamming_Weight_Operand1_Previous_Instruction",
+        "Hamming_Weight_Operand1_Subsequent_Instruction",
+        "Hamming_Weight_Operand2_Previous_Instruction",
+        "Hamming_Weight_Operand2_Subsequent_Instruction",
+        "Operand1",
+        "Operand1_Bit_Interactions"
+        "Operand2",
+        "Operand2_Bit_Interactions",
+        "Previous_Instruction",
+        "Subsequent_Instruction"};
+
+//! @brief This function contains the mathematical calculations that generate
+//! the Traces.
+//! @returns The generated Traces for the target program.
+const ELMO2::Internal::Traces&
+ELMO2::Internal::Model_TEMPLATE::Generate_Traces() const
+{
+    // *** Place your code here ***
+    //! @note m_coefficients and m_execution can be made use of to generate the
+    //! traces.
+}
