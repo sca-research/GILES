fa4c86556dba7ef9733f6effaecf6d10e21f5e64
Scott Egerton <scott.egerton@bristol.ac.uk>
2018-03-21 15:31:47 +0000

    Added Unit Test framework
    Made use of Catch2 to create unit tests
    (https://github.com/catchorg/Catch2)
    
    Integrated Catch2 with CMake

---
+++ test/Tests.cpp
@@ -0,0 +1,30 @@
+/*
+    This file is part of ELMO-2.
+
+    ELMO-2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ELMO-2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with ELMO-2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*!
+    @file Tests.cpp
+    @brief Contains the entry point for the tests
+    @author Scott Egerton
+    @date 2017-2018
+    @copyright GNU Affero General Public License Version 3+
+*/
+
+//! Required when using Catch testing framework
+//! @see https://github.com/catchorg/Catch2
+#define CATCH_CONFIG_MAIN
+
+#include <catch.hpp> // for catch
