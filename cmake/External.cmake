51fa89d95d23269cc1af0f79c26e1414cf2dbeb7
Scott Egerton <scott.egerton@bristol.ac.uk>
2018-12-14 15:26:50 +0000

    Updated CMake files
    Many tweaks and changes.
    Mainly: added a macro to update git submodules.

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -17,15 +17,15 @@
 
 # This option adds all dependencies to be re-downloaded as a method of updating
 # them. Dependencies that had not yet been downloaded will be downloaded.
-option(${PROJECT_NAME}_UPDATE_EXTERNAL_PROJECTS "Force re-download all external project files (Requires Git)"
-        OFF)
+option(${PROJECT_NAME}_UPDATE_EXTERNAL_PROJECTS "Force re-download all external\
+    project files (Requires Git)"
+    OFF) #TODO: Attach this to update all submodules command found online
 
 # This macro will clone the git submodule given by NAME into EXTERNAL_PROJECT_DIR if
 # it is not already there and include the directory within that project given by
 # INCLUDE_DIRECTORY in the building of TARGET.
 macro(target_include_external_project TARGET NAME INCLUDE_DIRECTORY)
-    if(${PROJECT_NAME}_UPDATE_EXTERNAL_HEADERS OR
-        NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME})
+    if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME})
 
         # Git is required to clone the submodules. Ensure this is found first.
         find_package(Git REQUIRED)
@@ -42,5 +42,33 @@ macro(target_include_external_project TARGET NAME INCLUDE_DIRECTORY)
 
         target_include_directories(${TARGET} SYSTEM PRIVATE
                                 ${EXTERNAL_PROJECT_DIR}/${NAME}/${INCLUDE_DIRECTORY})
+
+        if(${PROJECT_NAME}_UPDATE_EXTERNAL_PROJECTS)
+            update_external_submodules()
+        endif()
     endif()
+endmacro()
+
+macro(get_submodule NAME)
+    if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME})
+
+        # Git is required to clone the submodules. Ensure this is found first.
+        find_package(Git REQUIRED)
+
+        # Checks whether the directory given by EXTERNAL_PROJECT_DIR exists, if it
+        # does not then CMake will create it.
+        if(NOT EXISTS ${EXTERNAL_PROJECT_DIR})
+            file(MAKE_DIRECTORY ${EXTERNAL_PROJECT_DIR})
+        endif()
+
+        # Clone/Update the submodule
+        execute_process(COMMAND git submodule update --init -- ${NAME}
+                        WORKING_DIRECTORY ${EXTERNAL_PROJECT_DIR})
+    endif()
+endmacro()
+
+macro(update_external_submodules)
+        # Update all submodules to their latest commit.
+        execute_process(COMMAND git submodule update --quiet --recursive --init --remote
+                        WORKING_DIRECTORY ${EXTERNAL_PROJECT_DIR})
 endmacro()
